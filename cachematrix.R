f8e58f46907e90d792c35aea4ec628befd5725cc
Hal Peterson <vobine@users.noreply.github.com>
2014-05-25 14:54:03 -0500

    Implement makeCacheMatrix, with header comment and preamble comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2:
+##
+## Compute inverse of a matrix only when necessary;
+## if possible, return the cached result of a previous computation instead.
 
-## Write a short comment describing this function
+## makeCacheMatrix: initialize a structure to hold a matrix, a cache of its computed
+## inverse, and methods to get and set their values. The methods are:
+##  get()  - return the matrix
+##  set(x)  - assign a new matrix value
+##  getSol()  - return cached inverse (Solution)
+##  setSol(s)  - assign a new vaule to serve as the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setSol <- function(solution) inv <<- solution
+  getSol <- function() inv
+  
+  list(set = set, get = get,
+       setSol = setSol,
+       getSol = getSol)
 }
 
 
